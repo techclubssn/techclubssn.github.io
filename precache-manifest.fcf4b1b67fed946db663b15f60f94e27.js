27a2ece60f8b3054f985cc8b6c54ad0f9aabd68a
thatbrguy <bharathrajn98@gmail.com>
2019-01-04 20:03:53 +0530

    push files

---
+++ precache-manifest.fcf4b1b67fed946db663b15f60f94e27.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "53998b8b47cd4b6bd1c2",
+    "url": "/static/js/main.53998b8b.chunk.js"
+  },
+  {
+    "revision": "3f8679e3901212877209",
+    "url": "/static/js/1.3f8679e3.chunk.js"
+  },
+  {
+    "revision": "53998b8b47cd4b6bd1c2",
+    "url": "/static/css/main.af32a5dd.chunk.css"
+  },
+  {
+    "revision": "f380e7d293aadf2f213a49c4c5be9640",
+    "url": "/index.html"
+  }
+];
